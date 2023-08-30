53ea6ac1fbb725bf9fa5bf75ebd23b6be60d616a
juandiego-sierra-globant <141780480+juandiego-sierra-globant@users.noreply.github.com>
2023-08-30 17:42:16 -0500

    Delete Jenkins_Exercise directory

--- Jenkins_Exercise/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String variableValue = System.getenv("variableName");
-        System.out.println("Mensaje con variableeee de entorno: " + variableValue);
-    }
-}
