dd67d9787efa92f471be5931e9f21de971d8c30d
Juan Diego Sierra <juandiego.sierra@globant.com>
2023-08-30 12:44:20 -0500

    Changing message

--- Jenkins_Exercise/src/Main.java
+++ Jenkins_Exercise/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         String variableValue = System.getenv("variableName");
-        System.out.println("Mensaje con variabl e de entorno: " + variableValue);
+        System.out.println("Mensaje con variableeee de entorno: " + variableValue);
     }
 }
